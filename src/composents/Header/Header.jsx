5f1c53b518626a088885702b03a4d03dc2a5225d
Killian Barbarin <killianb31@gmail.com>
2025-04-12 16:03:06 +0200

    ADD logo and fonts

--- src/composents/Header/Header.jsx
+++ src/composents/Header/Header.jsx
@@ -2,16 +2,20 @@ import React from "react";
 
 import './Header.css';
 
-function Header () {
+function Header() {
     return (
         <header className="header">
-            <img src="./Small Logo.png" alt="logo" />
-            <h1>BCraft</h1>
-            <h1>Studio</h1>
-            <h2>A Propos</h2>
-            <h2>Services</h2>
-            <h2>Expertise</h2>
-            <button>Nous Contacter</button>
+            <div className="header-logo">
+                <img src="./Small Logo.png" className="header-logo-img" alt="logo" />
+                <h1 className="header-logo-bcraft">BCraft</h1>
+                <h1 className="header-logo-studio">Studio</h1>
+            </div>
+            <nav className="header-nav">
+                <a className="header-nav-link">À Propos</a>
+                <a className="header-nav-link">Services</a>
+                <a className="header-nav-link">Expertise</a>
+                <button className="header-nav-button">Nous Contacter</button>
+            </nav>
         </header>
     )
 }
