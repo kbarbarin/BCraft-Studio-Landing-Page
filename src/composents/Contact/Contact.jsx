4c0bd015e998c75e56f4b9b5d25dd9d31ec02b1d
Killian Barbarin <killianb31@gmail.com>
2025-04-16 09:50:54 +0200

    ADD Contact part

--- src/composents/Contact/Contact.jsx
+++ src/composents/Contact/Contact.jsx
@@ -4,7 +4,16 @@ import './Contact.css';
 function Contact () {
     return (
         <section className="contact">
-
+            <form className="contact-form">
+                <h1>Parlons de votre projet</h1>
+                <label>Nom complet</label>
+                <input type="text"/>
+                <label>Email</label>
+                <input type="text"/>
+                <label>Message</label>
+                <textarea rows="5"/>
+                <input type="submit" value="Envoyer le message" />
+            </form>
         </section>
     );
 }
