2018c1191cc7bc14248b01c331a64f605cb53115
Killian Barbarin <killianb31@gmail.com>
2025-04-17 17:19:17 +0200

    ADD Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import Missions from './composents/Missions/Missions';
 import Knowledge from './composents/Knowledge/Knowledge';
 import Contact from './composents/Contact/Contact';
+import Footer from './composents/Footer/Footer';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Missions />
       <Knowledge />
       <Contact />
+      <Footer />
     </>
   )
 }
---
+++ src/composents/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Footer.css';
+
+function Footer () {
+    return (
+        <footer className="footer">
+            <p className="footer-text">© 2025 BCraft Studio. Tous droits réservés. </p>
+            <a className="footer-link" href="#"> Mentions légales.</a>
+        </footer>
+    );
+}
+
+export default Footer;
